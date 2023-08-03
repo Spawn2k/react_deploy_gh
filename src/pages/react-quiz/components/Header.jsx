359b12a0e81cafe85c2db3cc52d3c806e6054fbf
Spawn2k <johndoe@example.com>
2023-08-03 20:39:01 +0200

    img fixed 2

--- src/pages/react-quiz/components/Header.jsx
+++ src/pages/react-quiz/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="app-header">
-      <img src="react_deploy_gh/assets/img/logo512.png" alt="React logo" />
+      <img src="/react_deploy_gh/assets/img/logo512.png" alt="React logo" />
       <h1>The React Quiz</h1>
     </header>
   );
