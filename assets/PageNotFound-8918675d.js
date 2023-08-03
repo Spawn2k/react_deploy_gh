b2ae50c4e9b5a080c222044270b7772b1eb8a5c3
Spawn2k <johndoe@example.com>
2023-08-03 18:08:43 +0200

    Updates

---
+++ assets/PageNotFound-8918675d.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-802466d4.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
--- assets/PageNotFound-d7b30ad2.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-064b9670.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
