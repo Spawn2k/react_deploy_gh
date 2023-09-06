fccb72751d3b5fee45b6ecd825cdcb58c059cb68
Duy Tuan <duytuanvn1986@gmail.com>
2023-09-06 21:26:55 +0200

    remove active-nav

--- src/data/projekts.jsx
+++ src/data/projekts.jsx
@@ -242,31 +242,31 @@ const projekts = [
       },
     ]
   },
-  {
-    title: 'Active - Nav',
-    link: 'https://spawn2k.github.io/js-active-link/',
-    level: 2,
-    react: false,
-    date: '09 - 2023',
-    description: 'Active Navbar mit schöner Animation leider noch mit kleinen bug',
-    icon: (<svg height="1em"
-    width="1em" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" >
-    <g id="Interface / External_Link">
-    <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-    </g>
-    </svg>),
-    tags: [
-      {
-        name: 'onScroll',
-        link: 'https://www.youtube.com/watch?v=UoE1hqfpXX0&t=2s&ab_channel=Codehal',
-      },
-      {
-        name: 'Tab animation',
-        link: 'https://www.youtube.com/watch?v=h2AOXBeVrgI&ab_channel=KevinPowell',
-      },
+  // {
+  //   title: 'Active - Nav',
+  //   link: 'https://spawn2k.github.io/js-active-link/',
+  //   level: 2,
+  //   react: false,
+  //   date: '09 - 2023',
+  //   description: 'Active Navbar mit schöner Animation leider noch mit kleinen bug',
+  //   icon: (<svg height="1em"
+  //   width="1em" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" >
+  //   <g id="Interface / External_Link">
+  //   <path id="Vector" d="M10.0002 5H8.2002C7.08009 5 6.51962 5 6.0918 5.21799C5.71547 5.40973 5.40973 5.71547 5.21799 6.0918C5 6.51962 5 7.08009 5 8.2002V15.8002C5 16.9203 5 17.4801 5.21799 17.9079C5.40973 18.2842 5.71547 18.5905 6.0918 18.7822C6.5192 19 7.07899 19 8.19691 19H15.8031C16.921 19 17.48 19 17.9074 18.7822C18.2837 18.5905 18.5905 18.2839 18.7822 17.9076C19 17.4802 19 16.921 19 15.8031V14M20 9V4M20 4H15M20 4L13 11" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+  //   </g>
+  //   </svg>),
+  //   tags: [
+  //     {
+  //       name: 'onScroll',
+  //       link: 'https://www.youtube.com/watch?v=UoE1hqfpXX0&t=2s&ab_channel=Codehal',
+  //     },
+  //     {
+  //       name: 'Tab animation',
+  //       link: 'https://www.youtube.com/watch?v=h2AOXBeVrgI&ab_channel=KevinPowell',
+  //     },
      
-    ]
-  },
+  //   ]
+  // },
   {
     title: 'Tabs',
     link: 'https://spawn2k.github.io/js-active-link/tab',
