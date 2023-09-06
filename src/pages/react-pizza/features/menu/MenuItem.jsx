d6a0e3ab5bfe4eabfed2f4a3dd7185077abab181
Duy Tuan <duytuanvn1986@gmail.com>
2023-09-06 10:35:06 +0200

    pizza menu responsive

--- src/pages/react-pizza/features/menu/MenuItem.jsx
+++ src/pages/react-pizza/features/menu/MenuItem.jsx
@@ -23,12 +23,12 @@ function MenuItem({ pizza }) {
   };
 
   return (
-    <li className="flex gap-4 py-2">
+    <li className="items-center flex gap-4 py-2 ">
       <img src={imageUrl} alt={name} className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`} />
       <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
         <p className="text-sm capitalize italic text-stone-500">{ingredients.join(', ')}</p>
-        <div className="mt-auto flex items-center justify-between">
+        <div className="add-to-cart mt-auto flex items-center justify-between">
           {!soldOut ? (
             <p className="text-sm">{formatCurrency(unitPrice)}</p>
           ) : (
